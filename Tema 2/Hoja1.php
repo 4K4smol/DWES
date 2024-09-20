0ce2ada99b4daca41ff2564b8cf4718c689677e6
Etsiu7 <eloycuesta@hotmail.es>
2024-09-20 20:05:16 +0200

    primera push2

---
+++ Tema 2/Hoja1.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoja 1</title>
+</head>
+<body>
+    <?php
+        $nombre = 'Eloy';
+        $ciudad = 'Torrelavega';
+
+        echo "<p><strong>$nombre</strong> $ciudad</p>";
+        $x = 1;
+        $y = 9;
+        $resultado = $x+$y;
+        echo "<p>$resultado</p>";
+
+        $x2 = -1;
+        $y2 = 9;
+        $suma = $x2  + $y2;
+
+        print ("El valor de x es <i>$x2<i>");
+        echo "<br/>";
+        
+        
+        print ("(El valor de y es <i>$y2</i>)<br/>");
+        print("La suma es <b><i>$suma</i></b><br/>");
+        echo "a"
+    ?>
+        
+ 
+
+
+</body>
+</html>
---
+++ Tema 2/hoja2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoja 2</title>
+</head>
+<body>
+<?php
+
+print ("Este es el resultado correcto del primer ejercicio");
+
+print("<br/>");
+print("Segundo ejercicio: visualización del contenido de variables");
+$nombre="yo";
+$edad=25;
+print("<p>Mi nombre es $nombre y mi edad es $edad</p>");
+
+?>
+</body>
+</html>
