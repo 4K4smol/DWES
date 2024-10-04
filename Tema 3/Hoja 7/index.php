8e34e1f4b23337a3c88235599b607e29a0f7f8a8
Etsiu7 <eloycuesta@hotmail.es>
2024-10-04 19:26:35 +0200

    correccion comentarios

--- Tema 3/Hoja 7/index.php
+++ Tema 3/Hoja 7/index.php
@@ -1,24 +1,19 @@
 <?php
 
-// Incluir los traits y la clase Empleado
 require_once 'Traits/InformacionPersonal.php';
 require_once 'Traits/InformacionLaboral.php';
 require_once 'Traits/Mensaje.php';
-require_once 'Clases/Empleado.php';  // Revisa que la carpeta y archivo sean correctos
+require_once 'Clases/Empleado.php';  
 
 use Hoja7\Clases\Empleado;
 
-// Crear un empleado
 $empleado = new Empleado();
 
-// Actualizar la información personal y laboral
 $empleado->actualizarInformacionPersonal("Juan Pérez", 30, "Calle Falsa 123");
 $empleado->actualizarInformacionLaboral("E12345", 3000.00);
 
-// Mostrar la información del empleado
 echo $empleado->mostrarInformacionCompleta();
 
-// Mostrar un mensaje adicional usando el trait Mensaje
 $empleado->mostrarMensaje("Información del empleado completada.");
 
 ?>
