4c16554189f630ac117a027fc9c0f4b405169de5
Etsiu7 <eloycuesta@hotmail.es>
2024-10-07 19:34:55 +0200

    composer

---
+++ Tema 3/z-composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '95b697fef1dcb9bc7f7aa4e4d497808284cc6039',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '95b697fef1dcb9bc7f7aa4e4d497808284cc6039',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'hackzilla/password-generator' => array(
+            'pretty_version' => '1.7.0',
+            'version' => '1.7.0.0',
+            'reference' => '743c8493d2c33ebf9496a1c3c248efeb568fe134',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hackzilla/password-generator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
