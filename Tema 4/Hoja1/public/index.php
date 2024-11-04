94152f74b127d97eb86e8d1e40e98029aeda98a8
aka.smol <53124536+4K4smol@users.noreply.github.com>
2024-11-04 20:07:50 +0100

    Update index.php

--- Tema 4/Hoja1/public/index.php
+++ Tema 4/Hoja1/public/index.php
@@ -20,7 +20,9 @@
     }
 
     $equipos = funcionesBD::getEquipos($connection);
+    if(isset($_POST['equipos'])){
     $jugadores = funcionesBD::getJugadoresEquipo($connection,$_POST['equipos']);
+    }
 
     ?>
 
@@ -31,7 +33,8 @@
         <select id="equipos" name="equipos">
             <?php 
                 foreach ($equipos as $equipo) {
-                    echo "<option value='{$equipo['nombre']}'>{$equipo['nombre']}</option>";
+                    $selected = (isset($_POST["equipos"]) && $_POST["equipos"] === $equipo["nombre"]) ? 'selected' : '';
+                    echo "<option value='{$equipo['nombre']}' $selected>{$equipo['nombre']}</option>";
                 }
             ?>
         </select>
@@ -41,7 +44,7 @@
     <hr>
 
     <?php 
-    
+        
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['Mostrar'])) {
 
             echo "<table><tr><td>NOMBRE<hr></td><td>PESO<hr></td></tr>";
@@ -52,6 +55,68 @@
 
         }
     
+    ?>
+
+
+    <h1>Traspasos de Jugadores</h1>
+    <hr>
+    <h2>Baja y alta de jugadores:</h2>
+    <hr>
+    <form method="post">
+        <label for="jugadorBaja">Baja de Jugador</label>
+        <select id="jugadorBaja" name="jugadorBaja">
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['Mostrar'])) {
+                    foreach($jugadores as $jugador){
+                        $selected = (isset($_POST["jugadorBaja"]) && $_POST["jugadorBaja"] === $jugador["nombre"]) ? 'selected' : '';
+                        echo "<option value='{$jugador['nombre']}' $selected>{$jugador['nombre']}</option>";                
+                    }
+                }
+            ?>
+        </select>
+        <h3>Datos del nuevo Jugador</h3>
+        <hr>
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre"><br><br>
+        <label for="procedencia">Procedencia:</label>
+        <input type="text" id="procedencia" name="procedencia"><br><br>
+        <label for="altura">Altura:</label>
+        <input type="number" id="altura" name="altura"><br><br>
+        <label for="peso">Peso:</label>
+        <input type="number" id="peso" name="peso"><br><br>
+        <label for="posicion">Posicion:</label>
+        <input type="text" id="posicion" name="posicion"><br><br>
+        <button type="submit" name="realizar">Realizar traspaso</button>
+    </form>
+    
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['realizar'])) {
+            if(
+                isset($_POST['nombre']) &&
+                isset($_POST['procedencia']) &&
+                isset($_POST['altura']) &&
+                isset($_POST['peso']) &&
+                isset($_POST['posicion'])
+                ){
+
+                if(isset($_POST['equipos'])){
+                    $equipoCopia=$_POST['equipos'];
+                }
+                    
+                if(isset($_POST['jugadorBaja'])){
+                    funcionesBD::darBaja($connection, $_POST['jugadorBaja']);
+                    funcionesBD::traspaso(
+                        $connection,
+                        $_POST['nombre'],
+                        $_POST['procedencia'],
+                        $_POST['altura'],
+                        $_POST['peso'],
+                        $_POST['posicion'],
+                        $equipoCopia);
+                }
+            }
+        }
+        
     ?>
 
 </body>
